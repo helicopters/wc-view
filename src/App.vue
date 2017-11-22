7c2e3837327dd49655553f95c70956abab38af08
林丰 <linhui@linfengdeMacBook-Air.local>
2017-11-23 02:16:49 +0800

    图片浏览功能实现.

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,24 @@
 	position: relative;
 	transform-origin: left top;
 	transition: all 333ms;
-	// left: 100px;
-
+	
 	/* 不允许用户自己定义这个玩意. 如果定义, 结果自己负责. */
-	// margin: 10px;
+	
 }
-
-
-
 .box {
-	// position:fixed;
-	// top: 0;
-	// right: 0;
-	// left: 0;
-	// bottom: 0;
-	// height:100%;
-	// width: 100%;
-	// opacity: 0.8;
-	// background: black;
-	// z-index: 100;
-
-	// display: flex;
-	// align-items:center;
-	// justify-content:center;
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	position: fixed;
 	top:0;
 	right: 0;
@@ -42,28 +36,90 @@
 	opacity: 0;
 	display: none;
 	transition: all 333ms;
-
 }
-
 .box-show {
 	display: block;
 }
 .box-diff {
-	// display
+	
 	opacity: 1;
+}
+
+.bb {
+	height: 200px;
+	width: 200px;
+}
+.cc {
+	height: 100px;
+	width: 150px;
+}
+.aa {
+	height: 300px;
+	width: 222px;
+}
+
+
+
+
+.wc-swiper {
+	// position: absolute!important;
+	// top:0;
+	// bottom: 0;
+	// left: 0;
+	// right: 0;
+	// background: red;
+	// display: flex;
+	// align-items:center;
+	// position: fixed!important;
+	// background: black;
+	z-index: 100001!important;
+	height: 100%;
+}
+
+.shishi {
+	width: 100%;
+	z-index: 1000000!important;
+	position: relative;
+}
+
+.wc-slide {
+	// position: absolute!important;
+	// top:0;
+	// bottom: 0;
+	// left: 0;
+	// right: 0;
+	// background: red;
+	display: flex;
+	align-items:center;	
 }
 
 
 </style>
 <template>
 	<div class="container">
+
+
+
+
 		<h1 v-for="i in 19">ff</h1>
-		<img src="https://bucket-zb-fanba.oss-cn-hangzhou.aliyuncs.com/4b4c9042e3754101ae11d4d97882cbc1.jpg" alt="" class="img">
+
+		<img :src="url" v-for="(url,key) in imgList" :key="key" alt="" class="img">
+
+
 		<p>11222</p>
 		<h1 v-for="i in 19">ff</h1>
+		<div class="box">
+			
 
-		<div class="box"></div>
+		<wc-swiper class="wc-swiper" :autoplay="false" v-if="showSwiper" :defaultSlide="xxx">
+			<wc-slide v-for="(value, key) in imgList" class="wc-slide">
+				<img :src="value" alt="" class="shishi">
+			</wc-slide>
+		</wc-swiper>
+			
 
+
+		</div>
 	</div>
 </template>
 <script>
@@ -71,6 +127,8 @@
 		name: 'App',
 		data () {
 			return {
+				xxx: 0,
+				showSwiper: false,
 				width: 0,
 				height: 0,
 				naturalHeight: 0,
@@ -79,204 +137,221 @@
 				position: '',
 				zIndex: '',
 				left: 0,
-				top: 0
+				top: 0,
+				current: null,
+				imgList: ['https://bucket-zb-fanba.oss-cn-hangzhou.aliyuncs.com/4b4c9042e3754101ae11d4d97882cbc1.jpg','http://www.nvsay.com/uploads/allimg/161108/172-16110R2214b22.jpg','http://www.nvsay.com/uploads/allimg/161108/172-16110R2220UG.jpg','http://t10.baidu.com/it/u=1863417261,812716211&fm=173&s=FAA0804142D83674D72CC59D0300D083&w=640&h=359&img.JPEG']
 			}
 		},
 		mounted () {
 
-			let img = document.querySelector('.img');
 
-			img.onload = () => {
+			
+			/* 显示 box */
+			let showBox = () => {
+				/* 因为渲染的问题, 所以最好在 onload 结束, 其实就是异步*/
+				let box = document.querySelector('.box');
+				
+				box.classList.add('box-show');
+				setTimeout(()=>{
+					box.classList.add('box-diff')
+					// this.showSwiper = true;
+				}, 50);
+			}
 
-				// var nImg = img.cloneNode(true);
 
-				// console.log(nImg);
 
-				img.addEventListener('click', (e)=>{
+			let setImg = (img) => {
+				this.position = getComputedStyle(img).position;
+				this.zIndex = getComputedStyle(img).zIndex;
+				
+				img.style.zIndex = 100000;
 
-					e.stopPropagation();
+				if (this.position == 'static') {
+					img.style.position = 'relative';
+				}				
+			}
+
+
+			let getScale = (img) => {
+
+				let imgWidth = img.width;
+				let screenWidth = document.documentElement.clientWidth;
+				let realWidth = img.naturalWidth;
+				let radio = 0;
+				if (realWidth < screenWidth) {
+					
+					
+					radio = 1;
+				} else {
+					radio = screenWidth/imgWidth;
+				}	
+				return radio;
+			}
+
+
+
+			
+
+
+			function toArray(like) {
+				console.log(like)
+				return Array.prototype.slice.call(like);
+			}
+				
+			toArray(document.querySelectorAll('.img'))
+				.forEach((el,index)=>{
+					el.index = index;
+
+					console.log(el)
+
+
+			el.onload = () => {
+
+				el.addEventListener('click', (e)=>{
+
+
 
 					/* 首先要改变 box 的状态*/
+						
+					showBox();
 
-					let box = document.querySelector('.box');
+					/* 然后设置 img 的属性 */
 
-					box.classList.add('box-show');
-					setTimeout(()=>{
-						box.classList.add('box-diff')
-					}, 50);
-
-					/* 
-						然后改变图片的状态 
-						z-index 要变的很大; 
-						因为 z-index 只对定位属性生效, 所以还要判断定位属性; 
-
-						保存这两个值. 
-
-					*/
-					this.position = getComputedStyle(img).position;
-					this.zIndex = getComputedStyle(img).zIndex;
-
-					// if (this.zIndex)
-					img.style.zIndex = 100000;
-					if (this.position == 'static') {
-						img.style.position = 'relative';
-					}
-
-					/* 
-						现在就要改变图片的位置和大小
-
-						首先拿到图片显示的宽度;
-						再拿到屏幕的宽度
-
-						判断图片的真实宽度; 
-
-						如果真实宽度小于屏幕宽度, 就不要放大了
-						大于, 还是要放大; 
-
-						让img 放大; 
-
-						同时屏蔽掉原先设置的 left, top 值; 
-
-						然后计算图片的高度;
-						高度不能按照比例来; 
-
-						高度还是按照等比缩放的
-
-	we
-
-					*/
-
-					let imgWidth = img.width;
-
-					let screenWidth = document.documentElement.clientWidth;
-
-					let realWidth = img.naturalWidth;
-
-					let radio = 0;
-
-					if (realWidth < screenWidth) {
-						// 不要放大
-					} else {
-						radio = screenWidth/imgWidth;
-					}
-
-					// img.style.transform = 'scale(' + radio + ')';
+					setImg(e.target);
 
 
-					this.left = getComputedStyle(img).left;
+					/* 然后开始计算放大比例*/
+					let radio = getScale(e.target);
 
-					this.top = getComputedStyle(img).top;
+					// console.log(radio*el.height);
 
-					// this.
+					var aa = el.naturalHeight/(el.naturalWidth/document.documentElement.clientWidth);
 
-					// img.style.position = 'fixed'
-					img.style.left = 0;
-					img.style.top = 0;
+					let br = aa/el.height;
 
+					console.log(br);
 
-/*
-计算出一个位置; 
+					// el.style.height = 
 
 
 
-*/					
-					let top = img.getBoundingClientRect().top;
-
-					// // let height = 
-					// /* 拿到展示之后的大小*/
-					// let showedHeight = img.height * radio;
-
-					// let diff = document.
 
 
+					/* 以及设置元素的 translateY 值*/
 
-					console.log(img.getBoundingClientRect().top);
+					let top = e.target.getBoundingClientRect().top;
 
-					let diff = document.documentElement.clientHeight - img.height*radio;
-
-
-
-					let half = -diff/2;
-
-					console.log(half,top,Math.abs(top) == Math.abs(half));
-
-
-					if (Math.abs(top) == Math.abs(half)) {
-						// alert(1)
-						return;
-					} else {
-
-
+					let left = e.target.getBoundingClientRect().left;
 
 					
-						console.log('这里啊')
-
-					// 拿 top - half
-
-					// let end = top - half;
-					let end = Math.abs(top) - Math.abs(half);
-
-					// if (top - end > 0) {
-					// 	end = -end;
-					// }
-
-					if (top>0){
-						end = -end;
+					let diff = document.documentElement.clientHeight - aa;
+					let half = diff/2;
+					
+					if (Math.abs(top) == Math.abs(half)) {
+						
+						return;
 					} else {
-						end = Math.abs(top) + Math.abs(half)
+						
+						
+						
+						let end = Math.abs(top) - Math.abs(half);
+						
+						
+						
+						if (top>0){
+							end = -end;
+						} else {
+							end = Math.abs(top) + Math.abs(half)
+						}	
+
+						e.target.style.left = 0;
+						e.target.style.transform = 'translate(' + (-left) + 'px,' + (end) + 'px) scale(' + radio + ',' + br + ')';	
+
+						this.current = e.target;		
 					}
 
 
+					let tend = ()=>{
 
-					// console.log(top, half, top-half)
-
-					// console.log(top)
-					// if (top< 0)
-
-					img.style.transform = 'translateY(' + (end) + 'px) scale(' + radio + ')';
+						
+						this.showSwiper = true;
+						el.style.zIndex = this.zIndex;
 
 
-					// console.log(nImg)
+						// console.log(el.index)
+						this.xxx = el.index;
 
 
-}
 
+					}
+
+					el.addEventListener('transitionend', tend, false)
+
+					
+					
+					
+					
+					
 
 				}, false);
 			}
 
 
-			// document.querySelector('.')
+
+
+
+
+
+
+
+
+
+				})
+
+			
+
+
+			// let img = document.querySelector('.img');
+
+
+
+
+
+
+
+
+
+
+
+			
 			let box = document.querySelector('.box');
-
 			box.addEventListener('click', ()=>{
-				// alert(1)
-
+				
 				/* 先将mask 给干掉 */
 				box.classList.remove('box-diff');
-
 				let handler = ()=>{
 					box.classList.remove('box-show');
 					box.removeEventListener('transitionend', handler, false);
-
 				}
-
 				box.addEventListener('transitionend', handler, false);
-
-				// img.classList.remove('')
-
-				img.style.transform = 'scale(1)';
-
-				img.style.left = this.left;
-				img.style.top = this.top;
-
-				img.style.zIndex = this.zIndex;
-				img.style.position = this.position;
-
-
-
-
+				
+				this.current.style.transform = 'scale(1)';
+				
+				
+				this.current.style.zIndex = this.zIndex;
+				this.current.style.position = this.position;
 			}, false);
+
+
+
+
+
+
+
+
+
+
+
 
 		}
 	}
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 
-import wcView from './wc-view'
-Vue.use(wcView);
+// import wcView from './wc-view'
+// Vue.use(wcView);
+
+import wcSwiper from 'wc-swiper'
+import 'wc-swiper/style.css'
+Vue.use(wcSwiper);
+
 
 new Vue({
     el: '#app',
